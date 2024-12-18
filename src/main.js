c6600f93d26d9c0296b8eb9d62721b5c01b899dd
ayusito <davidayuso99@gmail.com>
2024-12-18 11:17:25 +0100

    Actualizado

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import "node_modules/@ionic/core/css/ionic.bundle.css";
+import "@ionic/core/css/ionic.bundle.css";
 import "./style.css";
 
 // Import PWA elements (for web capabilities)
-import { defineCustomElements } from "node_modules/@ionic/core/loader/index.js";
+import { defineCustomElements } from "@ionic/core/loader/index.js";
 // Initialize PWA elements
 defineCustomElements(window);
 
